159187f4573fa0e7155134e1f65a9f5c57f1e600
Daniel Schaffrath <daniel.schaffrath@mac.com>
2008-02-25 11:32:18 +0000

    flowgrind: added missing change in fg_socket.h for prior checkin
    git-svn-id: svn+ssh://svn.umic-mesh.net/umic-mesh/tools/flowgrind/trunk@3643 3d4b872d-e00e-0410-a2ce-a5c2cda4747c

--- src/fg_socket.h
+++ src/fg_socket.h
@@ -1,7 +1,16 @@
 #ifndef _FG_SOCKET_H
 #define _FG_SOCKET_H
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <netinet/tcp.h>
 #include <sys/socket.h>
+
+#ifndef TCP_CONG_MODULE
+#define TCP_CONG_MODULE 13
+#endif
 
 ssize_t read_exactly(int, void *, size_t);
 size_t read_until_plus(int d, char *buf, size_t nbytes);
@@ -17,7 +26,8 @@ int set_so_dscp(int fd, uint8_t);
 int set_so_elcn(int fd, int val);
 int set_so_icmp(int fd);
 int set_dscp(int sock, uint8_t dscp);
-int set_so_cork(int fd);
+int set_tcp_cork(int fd);
+int toggle_tcp_cork(int fd);
 int set_window_size(int, int);
 int set_window_size_directed(int, int, int);
 
