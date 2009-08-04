f92049664f53de04e3181b967082aaa8d30d0324
Carsten Wolff <carsten.wolff@rwth-aachen.de>
2009-08-04 14:06:04 +0000

    added a script that creates a tarball from an svn tag
    git-svn-id: svn+ssh://svn.umic-mesh.net/umic-mesh/projects/flowgrind/trunk@5274 3d4b872d-e00e-0410-a2ce-a5c2cda4747c

---
+++ dist.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+VERSION=$1
+
+if [ "$VERSION" = "" ]; then
+	echo "Usage: $0 VERSION\n    Where VERSION is the version number of an svn tag."
+	exit 1
+fi
+
+SVN=`which svn`
+if [ "$SVN" = "" ]; then
+	echo "The subversion command 'svn' is not installed."
+	exit 1
+fi
+
+AUTORECONF=`which autoreconf`
+if [ "$AUTORECONF" = "" ]; then
+	echo "Please install autotools (apt-get install autoconf automake libtool)."
+	exit 1
+fi
+
+$SVN export svn+ssh://svn.umic-mesh.net/umic-mesh/projects/flowgrind/tags/flowgrind-$VERSION
+if [ $? -ne 0 ]; then
+	echo "Exporting subversion tag 'flowgrind-$VERSION' failed."
+	exit 1
+fi
+
+cd flowgrind-$VERSION
+$AUTORECONF -i
+if [ $? -ne 0 ]; then
+	echo "'autoreconf -i' failed. See error messages above."
+	exit 1
+fi
+
+rm -r config.h.in~ autom4te.cache 
+cd ..
+tar -cjf flowgrind-$VERSION.tar.bz2 flowgrind-$VERSION
+if [ $? -ne 0 ]; then
+	echo "failed to create the tarball."
+	exit 1
+fi
+
+rm -r flowgrind-$VERSION
+
+echo "Success: Built tarball flowgrind-$VERSION.tar.bz2"
