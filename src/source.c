ea37bba4c3fd251d47e5348a44b356c4636eca03
Christian Samsel <samsel.samsel@rwth-aachen.de>
2010-12-15 15:46:34 +0000

    flowgrind: revert fix
    git-svn-id: svn+ssh://svn.umic-mesh.net/umic-mesh/projects/flowgrind/trunk@6842 3d4b872d-e00e-0410-a2ce-a5c2cda4747c

--- src/source.c
+++ src/source.c
@@ -197,7 +197,9 @@ int add_flow_source(struct _request_add_flow_source *request)
 		return -1;
 	}
 #endif
-
+#ifdef HAVE_LIBPCAP
+        fg_pcap_go(flow);
+#endif
 	if (!flow->source_settings.late_connect) {
 		DEBUG_MSG(4, "(early) connecting test socket");
 		connect(flow->fd, flow->addr, flow->addr_len);
@@ -207,8 +209,5 @@ int add_flow_source(struct _request_add_flow_source *request)
 
 	request->flow_id = flow->id;
 
-#ifdef HAVE_LIBPCAP
-		fg_pcap_go(flow);
-#endif
 	return 0;
 }
