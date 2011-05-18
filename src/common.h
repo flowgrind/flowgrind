4c8be7968991850317af43bc7b8db7d260bae7cb
Christian Samsel <samsel.samsel@rwth-aachen.de>
2011-05-18 10:43:22 +0000

    remove pthread barrier for mac os x, some improvements to autofoo
    git-svn-id: svn+ssh://svn.umic-mesh.net/umic-mesh/projects/flowgrind/trunk@6953 3d4b872d-e00e-0410-a2ce-a5c2cda4747c

--- src/common.h
+++ src/common.h
@@ -6,14 +6,15 @@
 #endif
 
 #include <limits.h>
-#include <netinet/in.h>
 #include <stdio.h>
 #include <sys/time.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
-#include <netinet/tcp.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/tcp.h>
 
 #define UNUSED_ARGUMENT(x) (void)x
 
