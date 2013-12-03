57a2a2ad4ece17e529e0fc76603473c74689363b
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-12-03 12:40:17 +0100

    Add GPL header in *.h

--- src/destination.h
+++ src/destination.h
@@ -1,7 +1,28 @@
-#ifndef __DESTINATION_H__
-#define __DESTINATION_H__
+/*
+ * destination.h - Routines used to setup a Flowgrind destination for a test
+ *
+ * Copyright (C) Christian Samsel <christian.samsel@rwth-aachen.de>, 2010-2013
+ * Copyright (C) Tim Kosse <tim.kosse@gmx.de>, 2009
+ * Copyright (C) Daniel Schaffrath <daniel.schaffrath@mac.com>, 2007-2008
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef _DESTINATION_H_
+#define _DESTINATION_H_
 
 void add_flow_destination(struct _request_add_flow_destination *request);
 int accept_data(struct _flow *flow);
 
-#endif //__DESTINATION_H__
+#endif /* _DESTINATION_H_ */
--- src/log.h
+++ src/log.h
@@ -1,7 +1,28 @@
+/*
+ * fg_log.h - Logging routines used by Flowgrind
+ *
+ * Copyright (C) Christian Samsel <christian.samsel@rwth-aachen.de>, 2010-2013
+ * Copyright (C) Tim Kosse <tim.kosse@gmx.de>, 2009
+ * Copyright (C) Daniel Schaffrath <daniel.schaffrath@mac.com>, 2007-2008
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef _LOG_H_
 #define _LOG_H_
 
-#define LOGGING_MAXLEN  255             /* maximum string length */
+#define LOGGING_MAXLEN  255	/* maximum string length */
 
 extern int log_type;
 
