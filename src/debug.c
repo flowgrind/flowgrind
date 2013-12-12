ca73b870bf20b41cff898897f6ff597e2e3ca013
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-12-12 12:23:44 +0100

    Use 'timespec' struct also in debug.[h|c] so achieve also here a nanosecond precision

--- src/debug.c
+++ src/debug.c
@@ -1,7 +1,7 @@
 /*
  * debug.c - Debugging routines for Flowgrind
  *
- * Copyright (C) Christian Samsel <christian.samsel@rwth-aachen.de>, 2010-2013
+ * Copyright (C) Alexander Zimmermann <alexander.zimmermann@netapp.com>, 2013
  * Copyright (C) Tim Kosse <tim.kosse@gmx.de>, 2009
  * Copyright (C) Daniel Schaffrath <daniel.schaffrath@mac.com>, 2007-2008
  *
@@ -19,13 +19,6 @@
  *
  */
 
-#include <stdio.h>
-#include <stdarg.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <time.h>
-#include <unistd.h>
-
 #include "debug.h"
 
 #ifdef DEBUG
@@ -33,6 +26,12 @@
 #ifdef HAVE_CONFIG_H
 #include "config.h"
 #endif /* HAVE_CONFIG_H */
+
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+
+#include "fg_time.h"
 
 inline void decrease_debuglevel()
 {
@@ -46,36 +45,33 @@ inline void increase_debuglevel()
 	printf("DEBUG_LEVEL=%d\n", debug_level);
 }
 
-static double time_diff(struct timeval *tv1, struct timeval *tv2)
-{
-
-	return (double)(tv2->tv_sec - tv1->tv_sec) +
-		(double)(tv2->tv_usec - tv1->tv_usec)/1e6;
-}
-
+/*
+ * Helper function for DEBUG_MSG macro. Returns as string the current time in
+ * seconds and nanoseconds since the Epoch together with the delta in time to
+ * the last fct. call and the delta in time since the first fct. call
+ */
 const char *debug_timestamp()
 {
-	struct timeval now = {.tv_sec = 0, .tv_usec = 0};
-	static struct timeval first = {.tv_sec = 0, .tv_usec = 0};
-	static struct timeval last = {.tv_sec = 0, .tv_usec = 0};
-	size_t len = 0;
+	struct timespec now = {.tv_sec = 0, .tv_nsec = 0};
+	static struct timespec first = {.tv_sec = 0, .tv_nsec = 0};
+	static struct timespec last = {.tv_sec = 0, .tv_nsec = 0};
 	static char buf[80];
 
-	gettimeofday(&now, NULL);
+	gettime(&now);
 
-	if (!first.tv_sec && !first.tv_usec)
+	if (!first.tv_sec && !first.tv_nsec)
 		last = first = now;
 
-	len = strftime(buf, sizeof(buf), "%Y/%m/%d %H:%M:%S",
-		       localtime(&now.tv_sec));
-	snprintf(buf+len, sizeof(buf)-len, ".%06ld [+%8.6lf] (%8.6lf)",
-		 (long)now.tv_usec, time_diff(&last, &now),
-		 time_diff(&first, &now));
+	ctimespec_r(&now, buf, sizeof(buf));
+	snprintf(buf+strlen(buf), sizeof(buf)-strlen(buf),
+		 " [+%8.6lf] (%8.6lf)",
+		 time_diff(&last, &now), time_diff(&first, &now));
+
 	last = now;
 	return buf;
 }
 
-#else
+#else /* DEBUG */
 
 void decrease_debuglevel() { }
 void increase_debuglevel() { }
