13ae398774f4fb29fe1edebe4e8a57a16a7d3023
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2013-12-20 07:52:23 +0100

    Change GPL Boilerplate
    * Say explicit that we are GPL v2 only
    * Apply tips from https://www.gnu.org/licenses/gpl-howto.en.html

--- src/fg_math.h
+++ src/fg_math.h
@@ -1,20 +1,22 @@
+/**
+ * @file fg_math.h
+ * @brief Routines for statistics and advanced traffic generation
+ */
+
 /*
- * math.h - Flowgrind helper routines for statistics and advanced traffic
- * generation
+ * Copyright (C) 2010-2013 Christian Samsel <christian.samsel@rwth-aachen.de>
  *
- * Copyright (C) Christian Samsel <christian.samsel@rwth-aachen.de>, 2010-2013
+ * This file is part of Flowgrind. Flowgrind is free software; you can
+ * redistribute it and/or modify it under the terms of the GNU General
+ * Public License version 2 as published by the Free Software Foundation.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
+ * Flowgrind distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  */
 
