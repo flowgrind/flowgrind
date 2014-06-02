8cddbcee8dad839581ef85868afdc770dbc0264a
Felix Rietig <felix.rietig@rwth-aachen.de>
2014-06-02 16:23:42 +0200

    [Reduced warnings] Added definition for use of asprintf()
    This removes previous compiler warnings (implicit declaration).

--- src/fg_argparser.c
+++ src/fg_argparser.c
@@ -25,6 +25,7 @@
     reasons why the executable file might be covered by the GNU General
     Public License.
 */
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
