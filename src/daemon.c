8c55c65aa44e6dd335ed6e9ffcb81653f49360e2
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2014-06-12 08:38:51 +0200

    Compile error on OS X - missing bracket

--- src/daemon.c
+++ src/daemon.c
@@ -658,7 +658,7 @@ int get_tcp_info(struct _flow *flow, struct _fg_tcp_info *info)
 	CPY_INFO_MEMBER(tcpi_reordering);
 #endif /* __LINUX__ */
 #else
-	memset(info, 0, sizeof(struct _fg_tcp_info);
+	memset(info, 0, sizeof(struct _fg_tcp_info));
 #endif /* (defined __LINUX__ || defined __FreeBSD__) */
 	return 0;
 }
