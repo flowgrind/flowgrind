b73b5971ffba4f8b5a3ccbc88f938d53d8941cff
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2014-07-22 19:44:41 -0400

    [Coding style] use tabs instead of whitespaces

--- scripts/travis.osx.install.deps.sh
+++ scripts/travis.osx.install.deps.sh
@@ -3,14 +3,14 @@
 # parse packages in $PACKAGES
 EXTRA_PKGS=
 for PACKAGE in $PACKAGES; do
-    case $PACKAGE in
-        gsl)
-            EXTRA_PKGS="gsl $EXTRA_PKGS"
-            ;;
-        libpcap)
-            # Apple distributes libpcap with OS X, no action needed
-            ;;
-    esac
+	case $PACKAGE in
+		gsl)
+			EXTRA_PKGS="gsl $EXTRA_PKGS"
+			;;
+		libpcap)
+			# Apple distributes libpcap with OS X, no action needed
+			;;
+	esac
 done
 
 set -x
