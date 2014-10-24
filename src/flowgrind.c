b9c5a917b1011a35ed9e7624e5b486d730b7f228
Puneeth Nanjundaswamy <Puneeth.Nanjundaswamy@gmail.com>
2014-10-24 21:43:16 +0200

    This patch makes dumping log to file and to screen mutually exclusive

--- src/flowgrind.c
+++ src/flowgrind.c
@@ -2538,6 +2538,7 @@ static void parse_general_option(int code, const char* arg, const char* opt_stri
 				  "(in seconds)", opt_string);
 		break;
 	case LOG_FILE_OPTION:
+		copt.log_to_stdout = false;
 		copt.log_to_file = true;
 		if (arg)
 			log_filename = strdup(arg);
@@ -2571,6 +2572,7 @@ static void parse_general_option(int code, const char* arg, const char* opt_stri
 				  arg, opt_string);
 		break;
 	case 'w':
+		copt.log_to_stdout = false;
 		copt.log_to_file = true;
 		break;
 	/* unknown option or missing option-argument */
