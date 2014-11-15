5760d2cfd18707198856ed49bea4e64f1ac5dd4f
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2014-11-14 19:17:54 -1000

    Fix doxygen comment of det_num_digits()

--- src/flowgrind.c
+++ src/flowgrind.c
@@ -1333,6 +1333,7 @@ static void set_column_unit(const char *unit, unsigned int nargs, ...)
  * Determines the length of the integer part of a decimal number.
  *
  * @param[in] value decimal number
+ * @return length of integer part
  */
 static inline size_t det_num_digits(double value)
 {
