7803d6bf30c9f1fb7fa59e888953d83f432e9ef4
Alexander Zimmermann <alexander.zimmermann@netapp.com>
2014-11-19 11:39:29 +0100

    Delete whitespaces in flowgrind.h

--- src/flowgrind.h
+++ src/flowgrind.h
@@ -71,64 +71,64 @@ enum tcp_stack_t {
 /** Values for Linux tcpi_state, if not compiled on Linux. */
 enum tcp_ca_state {
 	/** TCP sender follows fast path execution (normal state). */
-        TCP_CA_Open = 0,
+	TCP_CA_Open = 0,
 	/** TCP sender receives duplicate ACKs or SACKs. */
-        TCP_CA_Disorder = 1,
+	TCP_CA_Disorder = 1,
 	/** TCP sender receives ECN congestion notifications. */
-        TCP_CA_CWR = 2,
+	TCP_CA_CWR = 2,
 	/** TCP sender performs Fast Recovery. */
-        TCP_CA_Recovery = 3,
+	TCP_CA_Recovery = 3,
 	/** Retransmission timeout occurred. */
-        TCP_CA_Loss = 4,
+	TCP_CA_Loss = 4,
 };
 #endif /* HAVE_TCP_CA_STATE */
 
 /** IDs to explicit address an intermediated interval report column. */
 enum column_id {
-        /** Flow ID. */
-        COL_FLOW_ID = 0,
-        /** Report interval. @{ */
-        COL_BEGIN,
-        COL_END,                                            /** @} */
-        /** Throughput per seconds. */
-        COL_THROUGH,
-        /** Transactions per second. */
-        COL_TRANSAC,
-        /** Blocks per second. @{ */
-        COL_BLOCK_REQU,
-        COL_BLOCK_RESP,                                     /** @} */
-        /** Application level round-trip time. @{ */
-        COL_RTT_MIN,
-        COL_RTT_AVG,
-        COL_RTT_MAX,                                        /** @} */
-        /** Application level inter-arrival time. @{ */
-        COL_IAT_MIN,
-        COL_IAT_AVG,
-        COL_IAT_MAX,                                        /** @} */
-        /** Application level one-way delay. @{ */
-        COL_DLY_MIN,
-        COL_DLY_AVG,
-        COL_DLY_MAX,                                        /** @} */
-        /** Metric from the Linux / BSD TCP stack. @{ */
-        COL_TCP_CWND,
-        COL_TCP_SSTH,
-        COL_TCP_UACK,
-        COL_TCP_SACK,
-        COL_TCP_LOST,
-        COL_TCP_RETR,
-        COL_TCP_TRET,
-        COL_TCP_FACK,
-        COL_TCP_REOR,
-        COL_TCP_BKOF,
-        COL_TCP_RTT,
-        COL_TCP_RTTVAR,
-        COL_TCP_RTO,
-        COL_TCP_CA_STATE,
-        COL_SMSS,
-        COL_PMTU,                                           /** @} */
+	/** Flow ID. */
+	COL_FLOW_ID = 0,
+	/** Report interval. @{ */
+	COL_BEGIN,
+	COL_END,                                            /** @} */
+	/** Throughput per seconds. */
+	COL_THROUGH,
+	/** Transactions per second. */
+	COL_TRANSAC,
+	/** Blocks per second. @{ */
+	COL_BLOCK_REQU,
+	COL_BLOCK_RESP,                                     /** @} */
+	/** Application level round-trip time. @{ */
+	COL_RTT_MIN,
+	COL_RTT_AVG,
+	COL_RTT_MAX,                                        /** @} */
+	/** Application level inter-arrival time. @{ */
+	COL_IAT_MIN,
+	COL_IAT_AVG,
+	COL_IAT_MAX,                                        /** @} */
+	/** Application level one-way delay. @{ */
+	COL_DLY_MIN,
+	COL_DLY_AVG,
+	COL_DLY_MAX,                                        /** @} */
+	/** Metric from the Linux / BSD TCP stack. @{ */
+	COL_TCP_CWND,
+	COL_TCP_SSTH,
+	COL_TCP_UACK,
+	COL_TCP_SACK,
+	COL_TCP_LOST,
+	COL_TCP_RETR,
+	COL_TCP_TRET,
+	COL_TCP_FACK,
+	COL_TCP_REOR,
+	COL_TCP_BKOF,
+	COL_TCP_RTT,
+	COL_TCP_RTTVAR,
+	COL_TCP_RTO,
+	COL_TCP_CA_STATE,
+	COL_SMSS,
+	COL_PMTU,                                           /** @} */
 #ifdef DEBUG
-        /** Read / write status. */
-        COL_STATUS,
+	/** Read / write status. */
+	COL_STATUS,
 #endif /* DEBUG */
 	/** Number of elements in enum. Must be last element. */
 	NUM_COL,
@@ -254,30 +254,30 @@ struct cflow {
 
 /** Header of an intermediated interval report column. */
 struct column_header {
-        /** First header row: name of the column. */
-        const char* name;
-        /** Second header row: unit of the column. */
-        const char* unit;
+	/** First header row: name of the column. */
+	const char* name;
+	/** Second header row: unit of the column. */
+	const char* unit;
 };
 
 /** State of an intermediated interval report column. */
 struct column_state {
-        /** Dynamically turn an column on/off. */
-        bool visible;
-        /** How often the current column width was too high. */
-        unsigned int oversized;
-        /** Last width of the column. */
+	/** Dynamically turn an column on/off. */
+	bool visible;
+	/** How often the current column width was too high. */
+	unsigned int oversized;
+	/** Last width of the column. */
 	size_t last_width;
 };
 
 /** Intermediated interval report column. */
 struct column {
-        /** Unique column identifier. */
-        enum column_id type;
-        /** Column header (name and unit). */
-        struct column_header header;
-        /** State of the column. */
-        struct column_state state;
+	/** Unique column identifier. */
+	enum column_id type;
+	/** Column header (name and unit). */
+	struct column_header header;
+	/** State of the column. */
+	struct column_state state;
 };
 
 #endif /* _FLOWGRIND_H_ */
